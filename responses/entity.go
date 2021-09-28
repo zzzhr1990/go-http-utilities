b03a2ec17c6e656c6e13874753c9900fc2ff1e78
zzzhr <zzzhr@hotmail.com>
2021-09-29 00:37:26 +0800

    add new responses

--- responses/entity.go
+++ responses/entity.go
@@ -1,5 +1,7 @@
 package responses
 
+import "net/http"
+
 type ResponseEntity struct {
 	// Error() string
 	Reference string
@@ -10,4 +12,36 @@ type ResponseEntity struct {
 
 func (e *ResponseEntity) Error() string {
 	return e.Message
+}
+
+func CreateNewBadRequestResponse(refrence string, message string) *ResponseEntity {
+	return &ResponseEntity{
+		Status:    http.StatusBadRequest,
+		Message:   message,
+		Reference: refrence,
+	}
+}
+
+func CreateNewInternalErrorResponse(refrence string, message string) *ResponseEntity {
+	return &ResponseEntity{
+		Status:    http.StatusInternalServerError,
+		Message:   message,
+		Reference: refrence,
+	}
+}
+
+func CreateNewForbiddenResponse(refrence string, message string) *ResponseEntity {
+	return &ResponseEntity{
+		Status:    http.StatusForbidden,
+		Message:   message,
+		Reference: refrence,
+	}
+}
+
+func CreateNewUnauthorizedResponse(refrence string, message string) *ResponseEntity {
+	return &ResponseEntity{
+		Status:    http.StatusUnauthorized,
+		Message:   message,
+		Reference: refrence,
+	}
 }
